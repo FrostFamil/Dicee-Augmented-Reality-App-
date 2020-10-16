adc2c0938133c033d90e12b32e61733d70eeb1f2
Famil <frostgamingrespawn@gmail.com>
2020-10-17 00:44:27 +0200

    touch method added

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -43,17 +43,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         //adding light
         sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-
-        // Set the scene to the view
-        sceneView.scene = diceScene
-        
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
-            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-            sceneView.scene.rootNode.addChildNode(diceNode)
-        }
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -73,6 +62,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlane)
+            
+            if let hitResult = results.first {
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
+                    diceNode.position = SCNVector3(
+                        x: hitResult.worldTransform.columns.3.x,
+                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                        z: hitResult.worldTransform.columns.3.z
+                    )
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+                }
+            }
+        }
     }
     
     //gives width height to detected plane
